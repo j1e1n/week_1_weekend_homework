84d9e648e54079588f8154228fc1717d3a25d591
Author Name <jenwardhaugh@outlook.com>
2021-01-09 14:55:42 +0000

    test 3 and 4  passed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -9,7 +9,12 @@ def get_pet_shop_name(list):
 def get_total_cash(list):
     sum = list["admin"]["total_cash"]
     return sum
-    
+
+   
+
+def add_or_remove_cash(list, amount):
+    list["admin"]["total_cash"] += amount   
+    return amount
 
 
 
